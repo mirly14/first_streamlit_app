a2a6bca60932200646a003cd64b234183e039788
mirly14 <143029234+mirly14@users.noreply.github.com>
2023-08-29 14:36:05 -0600

    Update streamlit_app.py
    fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice) # put the fruit u want
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 
 
