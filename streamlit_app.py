3e2eeca20943a348aa89be07017adfa85c903d92
mirly14 <143029234+mirly14@users.noreply.github.com>
2023-08-29 08:36:04 -0600

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parentes New Healthy Dinner')
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg') 
+stresmlit.text(' 🥑🍞 Avocado Toast')
